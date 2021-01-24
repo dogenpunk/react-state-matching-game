25178a3c01a0b0259485db2d595136210de983c5
Matthew M. Nelson <dogenpunk@gmail.com>
2021-01-24 16:31:38 -0600

    Refactor

---
+++ refactor/components/Tile/Tile.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+import './Tile.css'
+
+const Tile = (props) => {
+
+    let styles = (props.selected || props.matched) ? { backgroundColor: props.color } : null
+    return (
+        <div className='Tile'
+             style={styles}
+             onClick={() => {props.handleTileClicked(props.id, props.color)}}
+        >
+            { props.selected || props.matched ? <props.svg /> : null }
+        </div>
+    )
+}
+
+export default Tile
